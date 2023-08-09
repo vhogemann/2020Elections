5fdc31e2e66957f2a3ccdb81feed78ea4f5a4d8a
Victor Hogemann <victor.hogemann@gmail.com>
2023-08-09 17:05:13 +0100

    Indexing to ES

--- CamaraDeputados/Index.fsx
+++ CamaraDeputados/Index.fsx
@@ -2,7 +2,9 @@
 #r "System.Security.Cryptography"
 #r "nuget: Elastic.Clients.Elasticsearch, 8.9.1"
 
-let YEAR_RANGE = seq { for i in 2000 .. 2023 do yield i }
+let YEAR_RANGE = seq { for i in 2000 .. 2001 do yield i }
+
+let INDEX_NAME = "deputies"
 
 open System
 open System.Text
@@ -11,6 +13,7 @@ open Elastic.Clients.Elasticsearch
 
 let baseUrl = "https://camaradosdeputadosgql-qboe6n5gda-uc.a.run.app/graphql"
 let camara = Camara.Client(baseUrl)
+let elasticsearch = ElasticsearchClient()
 
 type DeputyDocument = {
     legislatureId: string
@@ -27,10 +30,11 @@ type DeputyDocument = {
     expenseSupplierName: string
     expenseType: string
 } with
-    member this.id = 
-        let md5 = MD5.Create()
-        let bytes = Encoding.UTF8.GetBytes($"{this.deputyId}-{this.legislatureId}-{this.expenseType}-{this.expenseSupplierCnpjOrCpf}-{this.expenseDocumentDate}")
-        Guid(md5.ComputeHash(bytes)).ToString()
+    member this.id
+        with get() = 
+            let md5 = MD5.Create()
+            let bytes = Encoding.UTF8.GetBytes($"{this.deputyId}-{this.legislatureId}-{this.expenseType}-{this.expenseSupplierCnpjOrCpf}-{this.expenseDocumentDate}")
+            Guid(md5.ComputeHash(bytes)).ToString()
     static member Create (legislature:Model.Legislature) (deputy:Model.Deputy) (expense:Model.Expense) =
         {
             legislatureId = legislature.id
@@ -48,15 +52,25 @@ type DeputyDocument = {
             expenseType = expense.expenseType
         }
 
+let fetchDeputyExpenses (deputyId:int) (year:int) (month:Nullable<int>) =
+    match camara.GetDeputyExpenses(deputyId, year, month).deputies with
+    | [||] -> None
+    | deputies -> 
+        match deputies.[0].expenses with
+        | [||] -> None
+        | _ -> Some deputies.[0]
+
 let fetchExpensesForYear (year:int) =
-    let datetime = DateTime.Now
+    let datetime = DateTime(year, 1, 1)
     camara.GetLegislature(datetime).legislatures
     |> Seq.map (fun legislature ->
         printfn "Fetching deputies for legislature %s" legislature.id
         legislature.deputies
         |> Seq.map(fun deputy -> 
-            printfn "Fetching expenses for %s" deputy.name
-            camara.GetDeputyExpenses(deputy.id, year, Nullable()).deputies.[0])
+            printfn $"Fetching expenses for { deputy.name } {deputy.id}"
+            fetchDeputyExpenses deputy.id year (Nullable())
+        )
+        |> Seq.choose id
         |> Seq.filter (fun deputy -> deputy.expenses <> null && deputy.expenses.Length > 0)
         |> Seq.map (fun deputy -> 
             deputy.expenses
@@ -67,5 +81,34 @@ let fetchExpensesForYear (year:int) =
     )
     |> Seq.concat
 
-for expense in fetchExpensesForYear(2023) do
-    printfn "%A" expense
+// Create index if it doesn't exist
+if not (elasticsearch.Indices.Exists(INDEX_NAME).Exists) then
+    let response = 
+        elasticsearch.Indices.CreateAsync(INDEX_NAME)
+        |> Async.AwaitTask
+        |> Async.Catch
+        |> Async.RunSynchronously
+    match response with
+    | Choice1Of2 _ -> printfn $"Index {INDEX_NAME} created"
+    | Choice2Of2 ex -> failwith ex.Message
+else
+    printfn $"Index {INDEX_NAME} already exists"
+
+//bulk insert expenses
+for year in YEAR_RANGE do
+    let expenses = fetchExpensesForYear year
+    printfn $"Inserting expenses for year {year}"
+    elasticsearch.BulkAll(expenses, fun bulk ->
+        bulk
+            .Index(INDEX_NAME)
+            .BackOffTime("30s")
+            .BackOffRetries(2)
+            .RefreshOnCompleted()
+            .MaxDegreeOfParallelism(Environment.ProcessorCount)
+            .Size(100) |> ignore
+    ).Wait(
+        TimeSpan.FromMinutes(5), 
+        fun next -> 
+            printfn $"Inserting page {next.Page}"
+            ()
+    ) |> ignore
